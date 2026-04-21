d657900c84ce508235f2e3bee90e5b549046e6e3
SporeCloud <nahumhillier@gmail.com>
2026-04-21 14:52:52 +0100

    added GitHub build file & light UI test

--- app.R
+++ app.R
@@ -3,7 +3,9 @@ library(bslib)
 library(ggplot2)
 
 ui <- page_sidebar(
-
+    title = "SporeCloud.github.io",
+    window_title = "SporeCloud.github.io",
+    lang = "EN"
 )
 
 server <- function(input, output) {
